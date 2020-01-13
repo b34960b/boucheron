055b98241c66e4d2884df04b4677a455c1faf49b
b34960b@gmail.com <chugyeonglim@chugyeonglim-ui-MacBookPro.local>
2020-01-13 21:16:21 +0900

    20200113_2

--- js/boucheron_main.js
+++ js/boucheron_main.js
@@ -1,20 +1,5 @@
 (function($){
-  const win = $(window);
-  const gnb = $('.navigation');
-const viewBox = $('#viewBox');
-  const headBox = $('#headBox');
-  let viewBoxOff = viewBox.offset().top;
-
-  win.on('scroll', function(){
-    let winScroll = win.scrollTop();
-    if(winScroll > viewBoxOff){
-      headBox.addClass('action');
-      gnb.hide();
-    }else{
-      headBox.removeClass('action');
-      gnb.show();
-    }
-  });
+  
 
 
 
